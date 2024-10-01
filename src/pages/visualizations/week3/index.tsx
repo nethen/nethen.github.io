68761efcac0f049f794fd074be24209f5d4388ae
nethen <72537484+nethen@users.noreply.github.com>
2024-09-30 21:03:49 -0700

    feat: added test graph component

--- src/pages/visualizations/week3/index.tsx
+++ src/pages/visualizations/week3/index.tsx
@@ -8,6 +8,7 @@ import { SketchJS } from "@/components/visualizations/week2/SketchJS";
 import { Figure } from "@/components/Layout/Figure";
 import { useEffect } from "react";
 import { useLenis } from "lenis/react";
+import { W3Visualization1 } from "@/components/visualizations/week3/W3Visualization1";
 
 export default function Visualizations() {
   const { width = 0 } = useWindowSize();
@@ -51,23 +52,67 @@ export default function Visualizations() {
       >
         {width >= 1024 && isClient && (
           <nav className="sticky top-28 flex flex-col gap-y-r1 h-0">
-            <h2 className="text-m-lg lg:text-r-lg">02</h2>
+            <h2 className="text-m-lg lg:text-r-lg">03</h2>
             <ul className="">
               <li>
-                <Link href="#sketch-1a">SVG</Link>
+                <Link href="#about">About</Link>
               </li>
               <li>
-                <Link href="#sketch-1b">JS</Link>
+                <Link href="#1">Vis 1</Link>
+              </li>
+              <li>
+                <Link href="#2">Vis 2</Link>
+              </li>
+              <li>
+                <Link href="#3">Vis 3</Link>
+              </li>
+              <li>
+                <Link href="#4">Vis 4</Link>
               </li>
             </ul>
           </nav>
         )}
         <hgroup
           className="col-span-full lg:col-span-6 lg:col-start-2 text-m-lg lg:text-r-lg scroll-m-28"
-          id="sketch-1a"
+          id="about"
         >
-          <h2>Naps taken over Week 1</h2>
+          <h2>About the Data</h2>
           {/* <h3 className="opacity-50">Made with SVG elements</h3> */}
+        </hgroup>
+        <p className="col-span-full lg:col-start-2 lg:col-span-4 2xl:col-start-2 2xl:col-span-3 4xl:col-start-2 4xl:col-span-2 text-midground">
+          A nice review of the SVG coordinate system and elements. A notable
+          challenge was preventing distortion of elements when the viewport was
+          resized. Fixes involved a mix of relative units (%) and alignment
+          using the preserveAspectRatio attribute.
+        </p>
+        <hgroup
+          className="col-span-full lg:col-span-6 lg:col-start-2 text-m-lg lg:text-r-lg scroll-m-28"
+          id="1"
+        >
+          <h2>Global Sales by Genre and Platform</h2>
+          {/* <h3 className="opacity-50">Made with SVG elements</h3> */}
+        </hgroup>
+        <p className="col-span-full lg:col-start-2 lg:col-span-4 2xl:col-start-2 2xl:col-span-3 4xl:col-start-2 4xl:col-span-2 text-midground">
+          A nice review of the SVG coordinate system and elements. A notable
+          challenge was preventing distortion of elements when the viewport was
+          resized. Fixes involved a mix of relative units (%) and alignment
+          using the preserveAspectRatio attribute.
+        </p>
+        <section
+          className={clsx(
+            "grid grid-cols-subgrid col-span-full gap-y-m2 lg:gap-y-r2 lg:mb-r4"
+          )}
+        >
+          <Figure>
+            <W3Visualization1 />
+          </Figure>
+        </section>
+        {/* <hgroup
+          className="col-span-full lg:col-span-6 lg:col-start-2 text-m-lg lg:text-r-lg scroll-m-28"
+          id="2"
+        >
+          <h2>Sales Over Time by Platform and Genre</h2>
+          
         </hgroup>
         <p className="col-span-full lg:col-start-2 lg:col-span-4 2xl:col-start-2 2xl:col-span-3 4xl:col-start-2 4xl:col-span-2 text-midground">
           A nice review of the SVG coordinate system and elements. A notable
@@ -84,6 +129,50 @@ export default function Visualizations() {
             <SketchSVG />
           </Figure>
         </section>
+        <hgroup
+          className="col-span-full lg:col-span-6 lg:col-start-2 text-m-lg lg:text-r-lg scroll-m-28"
+          id="3"
+        >
+          <h2>Regional Sales vs. Platform</h2>
+          
+        </hgroup>
+        <p className="col-span-full lg:col-start-2 lg:col-span-4 2xl:col-start-2 2xl:col-span-3 4xl:col-start-2 4xl:col-span-2 text-midground">
+          A nice review of the SVG coordinate system and elements. A notable
+          challenge was preventing distortion of elements when the viewport was
+          resized. Fixes involved a mix of relative units (%) and alignment
+          using the preserveAspectRatio attribute.
+        </p>
+        <section
+          className={clsx(
+            "grid grid-cols-subgrid col-span-full gap-y-m2 lg:gap-y-r2 lg:mb-r4"
+          )}
+        >
+          <Figure>
+            <SketchSVG />
+          </Figure>
+        </section>
+        <hgroup
+          className="col-span-full lg:col-span-6 lg:col-start-2 text-m-lg lg:text-r-lg scroll-m-28"
+          id="4"
+        >
+          <h2>A Visual Story</h2>
+       
+        </hgroup>
+        <p className="col-span-full lg:col-start-2 lg:col-span-4 2xl:col-start-2 2xl:col-span-3 4xl:col-start-2 4xl:col-span-2 text-midground">
+          A nice review of the SVG coordinate system and elements. A notable
+          challenge was preventing distortion of elements when the viewport was
+          resized. Fixes involved a mix of relative units (%) and alignment
+          using the preserveAspectRatio attribute.
+        </p>
+        <section
+          className={clsx(
+            "grid grid-cols-subgrid col-span-full gap-y-m2 lg:gap-y-r2 lg:mb-r4"
+          )}
+        >
+          <Figure>
+            <SketchSVG />
+          </Figure>
+        </section> */}
       </article>
     </>
   );
